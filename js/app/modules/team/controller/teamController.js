0062a2464324823cfb0f59c9dc5c05b11b227e43
Alexandre Beaudoin <ofrogon@gmail.com>
2014-11-26 11:06:24 -0500

    New website master version

--- js/app/modules/team/controller/teamController.js
+++ js/app/modules/team/controller/teamController.js
@@ -1,2 +1,2 @@
-/*! uCtrl-Website - v0.0.1 - 2014-11-25 */
+/*! uCtrl-Website - v0.0.1 - 2014-11-26 */
 "use strict";angular.module("teamModule").controller("teamList",["$rootScope","$scope","$http",function(a,b,c){b.team={},b.team.software=[{firstName:"",lastName:"",email:"",post:"",text:"",picture:""}],b.team.hardware=[{firstName:"",lastName:"",email:"",post:"",text:"",picture:""}],c.get("data/team.json").then(function(c){b.team=c.data,window.setTimeout(function(){a.$emit("memberLoaded")},500)}),b.position=function(a){return""==a&&(a="N.A."),a}}]);
--- js/app/modules/team/team.js
+++ js/app/modules/team/team.js
@@ -1,2 +1,2 @@
-/*! uCtrl-Website - v0.0.1 - 2014-11-25 */
+/*! uCtrl-Website - v0.0.1 - 2014-11-26 */
 "use strict";!function(){angular.module("teamModule",[])}();
